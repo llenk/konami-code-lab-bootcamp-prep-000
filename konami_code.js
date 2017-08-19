199fe7a66dfe1f7f3cdff9bf0d84607e5df7e0e3
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 22:27:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,10 +4,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-  const main = document.body;
   var index = 0;
 
-  main.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
