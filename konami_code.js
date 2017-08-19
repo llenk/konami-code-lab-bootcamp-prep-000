198e1769cbbbc3f58086877f1f13cf10d325fdb0
Ellen Keal <ellenkeal@gmail.com>
2017-08-19 22:14:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,13 +5,13 @@ var index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === alphabet[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
